a877be266147f6ba7aaa9a3183811b069f068fdc
Le Wang <lewang@todaq-le-mba.local>
2026-07-18 08:51:32 -0400

    bump version to 10 as this fork has major changes.
    In the future I'll rename this.

--- buffer-flip.el
+++ buffer-flip.el
@@ -4,7 +4,7 @@
 ;; Keywords: convenience
 ;; URL: https://github.com/killdash9/buffer-flip.el
 ;; Created: 10th November 2015
-;; Version: 3.0
+;; Version: 10.0
 ;; Package-Requires: ((cl-lib "0.5"))
 
 ;; This program is free software; you can redistribute it and/or modify
